cdf04cf53ac69c687b4b7d9dd760b5ef4da0b2fb
KJ Kim <gencode.me@gmail.com>
2026-08-27 10:08:32 +0900

    fix: translate Craigslist posting body

--- src/content/block-collector.js
+++ src/content/block-collector.js
@@ -1,7 +1,7 @@
 import { hashSourceText } from './translation-queue.js';
 import { isTranslatableBlock } from './language.js';
 
-const BLOCK_SELECTOR = 'p,h1,h2,h3,h4,h5,h6,li,blockquote,figcaption,div,td,th';
+const BLOCK_SELECTOR = 'p,h1,h2,h3,h4,h5,h6,li,blockquote,figcaption,div,section,td,th';
 const EXCLUDED_CONTENT_SELECTOR = 'script,style,noscript,code,pre,input,textarea,select,button';
 const EXCLUDED_ANCESTOR_SELECTOR = `${EXCLUDED_CONTENT_SELECTOR},[contenteditable="true"],[contenteditable=""]`;
 const GENERATED_SELECTOR = 'translight-translation,[data-translight-generated="true"]';
